8fd489ce9282e0460d562bfd94e900a09a8573a4
Dessert99 <lsjh1234@naver.com>
2025-10-02 22:29:55 +0900

    feat(googlemap): 구글맵 구현

--- src/screens/map/MapHomeScreen.tsx
+++ src/screens/map/MapHomeScreen.tsx
@@ -1,17 +1,20 @@
-import { Text } from "react-native";
-import { SafeAreaView } from "react-native-safe-area-context";
-import DrawerButton from "../../components/DrawerButton";
-import { useAuth } from "@/hooks/queries/useAuth";
+import { StyleSheet } from "react-native";
+
+import MapView, { PROVIDER_GOOGLE } from "react-native-maps";
 
 const MapHomeScreen = () => {
-  const { logoutMutation } = useAuth();
   return (
-    <SafeAreaView>
-      <Text>MapHomeScreen</Text>
-      <DrawerButton />
-      <Text onPress={() => logoutMutation.mutate(null)}>로그아웃</Text>
-    </SafeAreaView>
+    <MapView
+      style={styles.container}
+      provider={PROVIDER_GOOGLE}
+    />
   );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+});
 
 export default MapHomeScreen;
