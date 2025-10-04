c3c272fc5d8905dc1a1bf0886f3353448508423c
Dessert99 <lsjh1234@naver.com>
2025-10-04 21:24:06 +0900

    feat(ui): 드로어 버튼 노치 영역 구해서 지도에 띄우기

--- src/components/DrawerButton.tsx
+++ src/components/DrawerButton.tsx
@@ -1,4 +1,4 @@
-import { Pressable, StyleSheet } from "react-native";
+import { Pressable, StyleProp, StyleSheet, ViewStyle } from "react-native";
 
 import { useNavigation } from "@react-navigation/native";
 import { DrawerNavigationProp } from "@react-navigation/drawer";
@@ -8,12 +8,17 @@ import { colors } from "../constants/colors";
 
 type Navigation = DrawerNavigationProp<MainDrawerParamList>;
 
-const DrawerButton = ({ color = colors.BLACK }) => {
+interface DrawerButtonProps {
+  color?: string;
+  style?: StyleProp<ViewStyle>; // 스타일을 props로 넘겨받을 수 있다.
+}
+
+const DrawerButton = ({ style, color = colors.BLACK }: DrawerButtonProps) => {
   const navigation = useNavigation<Navigation>(); // 이제 Drawer관련 함수들이 잘 추론된다.
 
   return (
     <Pressable
-      style={styles.container}
+      style={[styles.container, style]}
       onPress={() => navigation.openDrawer()}>
       <Ionicons
         name='menu'
