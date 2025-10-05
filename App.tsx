264ab0cba9cc2b90f2634f97a95f6db6bd4f3080
Dessert99 <lsjh1234@naver.com>
2025-10-05 22:09:50 +0900

    feat(map): 토스트 메시지 커스텀

--- App.tsx
+++ App.tsx
@@ -2,7 +2,42 @@ import RootNavigation from "./src/navigations/RootNavigation";
 import { SafeAreaProvider } from "react-native-safe-area-context";
 import { QueryClientProvider } from "@tanstack/react-query";
 import { queryClient } from "@/api/queryClient";
-import Toast from "react-native-toast-message";
+import Toast, {
+  BaseToast,
+  BaseToastProps,
+  ErrorToast,
+} from "react-native-toast-message";
+import { colors } from "@/constants/colors";
+
+// 토스트 메시지 커스텀
+const toastConfig = {
+  success: (props: BaseToastProps) => (
+    <BaseToast
+      {...props}
+      style={{ borderLeftColor: colors.BLUE_500 }}
+      text1Style={{
+        fontSize: 15,
+      }}
+      text2Style={{
+        fontSize: 12,
+      }}
+    />
+  ),
+  error: (props: BaseToastProps) => (
+    <ErrorToast
+      {...props}
+      style={{ borderLeftColor: colors.RED_500 }}
+      //제목 크기
+      text1Style={{
+        fontSize: 15,
+      }}
+      //설명 크기
+      text2Style={{
+        fontSize: 12,
+      }}
+    />
+  ),
+};
 
 const App = () => {
   return (
@@ -10,7 +45,7 @@ const App = () => {
       <SafeAreaProvider>
         <QueryClientProvider client={queryClient}>
           <RootNavigation />
-          <Toast />
+          <Toast config={toastConfig} />
         </QueryClientProvider>
       </SafeAreaProvider>
     </>
